017c66460e89f99edbef9666bb856b759dff8e04
Mr.Maulana <made03861@gmail.com>
2023-01-11 23:50:42 +0700

    commit first

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Ramsey\Uuid\Rfc4122\NilUuid;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(auth()->guest()){
+            abort(403);
+        }
+        return $next($request);
+    }
+}
